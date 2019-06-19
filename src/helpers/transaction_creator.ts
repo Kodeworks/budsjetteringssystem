ee2f32921416300491554fd216fce1662683e261
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2019-06-19 15:18:44 +0200

    Add dummy creator, and implemented into App and stories

---
+++ src/helpers/transaction_creator.ts
@@ -0,0 +1,36 @@
+import { ITransaction, TransactionType } from '../declarations/transaction';
+
+const words: Array<string> = [
+  'Otter',
+  'Gerbil',
+  'Fish',
+  'Party',
+  'Fluor',
+  'Hereditary',
+  'Consecutive',
+  'Sustenance',
+  'Party',
+  'Paycheck',
+  'Food',
+  'Drinks',
+];
+
+const randomWord = (): string => words[Math.floor(Math.random() * words.length)];
+
+const types: Array<TransactionType> = [
+  TransactionType.expense,
+  TransactionType.income,
+];
+
+const randomType = (): TransactionType => types[Math.floor(Math.random() * types.length)];
+
+let i: number = 0;
+
+export const createDummyTransaction = (): ITransaction => ({
+  companyId: 0,
+  date: new Date(Math.floor(Math.random() * 1500000000000)).toLocaleDateString(),
+  id: i++,
+  money: Math.random() * 10000,
+  name: `${randomWord()} ${randomWord()}`,
+  type: randomType(),
+});
