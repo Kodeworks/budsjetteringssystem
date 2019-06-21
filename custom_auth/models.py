ccbd6acfa4ad0c504648133ff23ac872a2fe30f1
Sindre Stephansen <sindre@sindrestephansen.com>
2019-06-21 14:26:59 +0200

    Fix code style

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractUser, Permission
+from django.contrib.auth.models import BaseUserManager, AbstractUser
 
 from company.models import Company
 from . import roles
