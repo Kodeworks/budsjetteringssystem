449b4943bcddf638799991cded71113b16cc4dd1
Sindre Stephansen <sindre@sindrestephansen.com>
2019-06-21 10:22:43 +0200

    Add generic views for handling company roles

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractUser, Permission
 
 from company.models import Company
+from . import roles
 
 
 class UserManager(BaseUserManager):
@@ -34,8 +35,9 @@ class User(AbstractUser):
         max_length=255,
         unique=True,
     )
-    company = models.ManyToManyField(
+    companies = models.ManyToManyField(
         Company,
+        through='UserCompanyThrough',
         related_name='users',
     )
 
@@ -43,3 +45,51 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def get_company_through(self, company):
+        """Get the object in the many-to-many relation to a company."""
+        if isinstance(company, int):
+            company_id = company
+        elif isinstance(company, Company):
+            company_id = company.pk
+        else:
+            return None
+
+        through_model = self.companies.through
+        try:
+            return through_model.objects.get(company=company_id, user=self)
+        except through_model.DoesNotExist:
+            return None
+
+    def get_role(self, company):
+        """Get the role this user has in the given company."""
+        through = self.get_company_through(company)
+
+        return through.role if through else None
+
+    def has_role(self, company, role):
+        """
+        Check that this user has a role in a company.
+
+        Returns True if `role` is None.
+        """
+        if role is None:
+            return True
+
+        return roles.is_equivalent(self.get_role(company), role)
+
+
+class UserCompanyThrough(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,
+    )
+    role = models.CharField(
+        choices=roles.choices,
+        max_length=2,
+        default=roles.USER,
+    )
--- custom_auth/views.py
+++ custom_auth/views.py
@@ -1,10 +1,138 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate
 from rest_framework import views, mixins, generics, permissions
+from rest_framework.exceptions import PermissionDenied, ParseError
 from rest_framework.response import Response
+from rest_framework.decorators import permission_classes
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from .models import User
 from .serializers import UserSerializer
+from . import roles
+
+
+class CompanyAccessView(generics.GenericAPIView):
+    """
+    Checks that the user has access to the the requested operation on/for a company.
+
+    The role required to perform an action is given by the `company_access` attribute.
+    The role is set for each type of HTTP method. This can be overwritten in subclasses.
+    """
+    permission_classes = (permissions.IsAuthenticated,)
+    company_id_field = 'company_id'
+    company_access = {
+        'GET': roles.REPORTER,
+        'POST': roles.USER,
+        'PUT': roles.USER,
+        'DELETE': roles.USER,
+    }
+    permissions = {}
+
+    def get_permissions(self):
+        """
+        Get the permissions given by `permission_classes`,
+        unless overriden in `permissions` for the current method.
+        """
+        if self.request.method in self.permissions:
+            permissions = self.permissions[self.request.method]
+            if permissions:
+                return [permission() for permission in permissions]
+            else:
+                return []
+        else:
+            return super().get_permissions()
+
+    def get_company_access(self):
+        """
+        Return the roles required for the different HTTP methods.
+
+        If a subclass redifines `company_access` it is merged with
+        `CompanyAccessView.company_access`.
+        """
+        return {
+            **CompanyAccessView.company_access,
+            **self.company_access,
+        }
+
+    def get_data(self, request):
+        """Get the data/parameters for the request."""
+        if request.method in ['GET', 'DELETE']:
+            return request.query_params
+        else:
+            return request.data
+
+    def get_company_id(self, request):
+        """
+        Get the ID of the current company.
+
+        The ID is passed in the field given by `company_id_field`
+        in either query or post data, depending on the type of request.
+
+        If no ID is passed None is returned.
+        """
+        data = self.get_data(request)
+
+        if self.company_id_field in data:
+            return data[self.company_id_field]
+        else:
+            return None
+
+    def check_company_role(self, request, *args, **kwargs):
+        """Check that the user has permission to do this action on the current company."""
+        company_id = self.get_company_id(request)
+        required_role = self.get_company_access()[request.method]
+
+        if not request.user.has_role(company_id, required_role):
+            raise PermissionDenied("You don't have access to do this operation on this company")
+
+    def initial(self, request, *args, **kwargs):
+        super().initial(request, *args, **kwargs)
+        self.check_company_role(request, *args, **kwargs)
+
+
+class SingleObjectQueryOrDataMixin:
+    """Allows pk for a object to be sent as a query or POST parameter, instead of through the URL."""
+    lookup_query_field = None
+    lookup_data_field = None
+    lookup_arg_field = None
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        if self.request.method == 'GET':
+            field = self.lookup_query_field or self.lookup_arg_field or self.lookup_field
+            data = self.request.query_params
+        else:
+            field = self.lookup_data_field or self.lookup_arg_field or self.lookup_field
+            data = self.request.data
+
+        if field not in data:
+            raise ParseError(f'Missing argument "{field}"')
+        value = data[field]
+
+        obj = get_object_or_404(queryset, **{self.lookup_field: value})
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+
+class RetrieveView(SingleObjectQueryOrDataMixin, mixins.RetrieveModelMixin, CompanyAccessView):
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+
+class RetrieveCreateUpdateDestroyView(SingleObjectQueryOrDataMixin, mixins.RetrieveModelMixin, mixins.CreateModelMixin,
+                                      mixins.UpdateModelMixin, mixins.DestroyModelMixin, CompanyAccessView):
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
 class Login(views.APIView):
