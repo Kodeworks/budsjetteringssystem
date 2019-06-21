4d464ca6f007d56442bfffd5ab0b36a97975ee50
Nicolaj Nielsen <nicolaj.niels1@gmail.com>
2019-06-21 15:25:02 +0200

    made draft of new theme object

--- src/styling/theme.ts
+++ src/styling/theme.ts
@@ -1,9 +1,29 @@
 export const theme = {
-  accent1        : '#f9f9f9',
-  accent2        : '#3c6578',
-  contrast       : '#0c374d',
-  emphasisShadow : 'box-shadow : 2px 2px 5px 0 #ccc;',
-  error          : '#ff6961',
-  main           : '#f6f6f2',
-  success        : '#03c03c',
+  palette: {
+    background: {
+      default: '#f6f6f2',
+      paper: '#fff',
+    },
+    error: {
+      contrast: '#fff',
+      main: '#ff6961',
+    },
+    primary: {
+      contrast: '#0c374d',
+      main: '#f9f9f9',
+    },
+    secondary: {
+      contrast: '#fff',
+      main: '#3c6578',
+    },
+    success: {
+      contrast: '#fff',
+      main: '#03c03c',
+    },
+  },
+  shadow: '2px 2px 5px 0 #ccc',
+  shape: {
+    borderRadius: '4px',
+  },
+  spacing: '8px',
 };
