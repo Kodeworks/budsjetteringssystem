eb927714151c79180631fe0fec5065f136f304b4
Sindre Stephansen <sindre@sindrestephansen.com>
2019-06-24 16:43:04 +0200

    Add missing auth urls

--- custom_auth/urls.py
+++ custom_auth/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import Login
+from .views import Login, UserView, UserByEmailView
 
 
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('', UserView.as_view(), name='user'),
+    path('byEmail/', UserByEmailView.as_view(), name='user-by-email'),
 ]
