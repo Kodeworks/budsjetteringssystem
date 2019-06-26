73112da7a0a14cb41d8d0719a16115aa78cb13ee
Sindre Stephansen <sindre@sindrestephansen.com>
2019-06-26 13:18:39 +0200

    Add url to set the role of a user

--- company/urls.py
+++ company/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import CompanyView, CompanyAddUserView, CompanyRemoveUserView
+from .views import CompanyView, CompanyAddUserView, CompanyRemoveUserView, CompanySetRoleView
 
 
 urlpatterns = [
     path('', CompanyView.as_view(), name='company'),
     path('addUser/', CompanyAddUserView.as_view(), name='company-add-user'),
     path('removeUser/', CompanyRemoveUserView.as_view(), name='company-remove-user'),
+    path('setRole/', CompanySetRoleView.as_view(), name='company-set-role'),
 ]
