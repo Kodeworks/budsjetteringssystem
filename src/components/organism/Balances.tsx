bdea5a1f5381f13e07fd59d2364d9282c8bb86f9
Orjan Vesterlid <orjanbostad.vesterlid@gmail.com>
2019-06-27 03:26:19 +0200

    WIP: Create BalanceEntries from transactions state filtered by month

--- src/components/organism/Balances.tsx
+++ src/components/organism/Balances.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 import {TransactionCtx} from '../../contexts/transaction';
 import {IBalanceEntry} from '../../declarations/balanceEntries';
+import { ITransaction } from '../../declarations/transaction';
 import MonthPicker from '../atoms/MonthPicker';
 import BalancesTable from '../molecules/BalancesTable';
 
@@ -11,14 +12,27 @@ interface IProps {
 }
 
 const Balances: React.FC<IProps> = props => {
-  const { store } = React.useContext(TransactionCtx);
-  const [month, setMonth] = React.useState(moment());
-
-  const entries: Array<IBalanceEntry> = [
+  const devEntries: Array<IBalanceEntry> = [
     {date: '1/6', income: 340000, expense: 100000, liquidity: 8430000},
     {date: '24/6', income: 100000, expense: 300000, liquidity: 8670000},
     {date: '25/6', income: 200050, liquidity: 8870000},
   ];
+
+  const { store } = React.useContext(TransactionCtx);
+  const [month, setMonth] = React.useState(moment());
+  const [entries, setEntries] = React.useState([]);
+
+  const filterDate = (transaction: ITransaction) => {
+    const transDate = moment(transaction.date);
+    return transDate.month() === month.month() && transDate.year() === month.year() ;
+  };
+
+  // Calculate new balance entries and update entries state when month changes.
+  React.useEffect(() => {
+    const transactions = store.transactions.filter(filterDate);
+    let entries = [];
+    
+    });
 
   return (
     <div className={props.className}>
--- src/stories/BalancesList.stories.tsx
+++ src/stories/BalancesList.stories.tsx
@@ -1,45 +1,10 @@
 import {storiesOf} from '@storybook/react';
 import React from 'react';
-
-import { GlobalStyle } from '../styling/global';
+import GlobalWrapper from '../helpers/GlobalWrapper';
 
 import BalancesTable from '../components/molecules/BalancesTable';
 import Balances from '../components/organism/Balances';
-import { createTransactionCtx } from '../contexts/transaction';
 import { IBalanceEntry } from '../declarations/balanceEntries';
-
-/*
-const Wrapper = props => {
-  return (
-    <GlobalStyle>
-      {props.children}
-    </GlobalStyle>
-  )
-};
-
-addDecorator(storyFn => (
-  <Wrapper>
-    {storyFn()}
-  </Wrapper>
-));
-const { store } = React.useContext(TransactionCtx);
-
-const Wrapper = props => {
-  const [store, dispatch] = React.useReducer(reducer, initialState);
-  createTransactionCtx(store, dispacth);
-
-  if (store.transactions.length === 0) {
-    const txEntries = (new Array(100)).fill(1).map(createDummyTransaction);
-    txEntries.map(e => dispatch(addTransaction(e)));
-  }
-
-  return (
-    <TransactionCtx.Provider value={{store, dispatch}}>
-      {props.children}
-    </TransactionCtx.Provider>
-  );
-}
-*/
 
 const entries: Array<IBalanceEntry> = [
   {date: '1/6', income: 340000, expense: 100000, liquidity: 8430000},
@@ -52,5 +17,7 @@ storiesOf('Balances', module)
     <Balances />
   ))
   .add('BalancesTable', () => (
-    <BalancesTable entries={entries} />
+    <GlobalWrapper>
+      <BalancesTable entries={entries} />
+    </GlobalWrapper>
   ));
