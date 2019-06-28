adca17afdde5fa5a9ad3a1680a85b6b2137925b2
Henriette Andersen <henriean@stud.ntnu.no>
2019-06-28 17:50:14 +0200

    Set up transaction view skeleton and add tests.

--- transaction/urls.py
+++ transaction/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import TransactionAllView
+from . import views
 
 urlpatterns = [
-    path('all/', TransactionAllView.as_view(), name='transaction-all')
+    path('', views.TransactionView.as_view(), name='transaction'),
+    path('all/', views.TransactionAllView.as_view(), name='transaction-all'),
+    path('byDate/', views.TransactionByDateView.as_view(), name='transaction-by-date'),
+    path('byDateRange/', views.TransactionByDateRangeView.as_view(), name='transaction-by-date-range'),
+    path('income/all/', views.TransactionIncomeAllView.as_view(), name='transaction-income-all'),
+    path('expense/all/', views.TransactionExpenseAllView.as_view(), name='transaction-expense-all'),
 ]
