fd26a60b60ab945d46817837ee48a1e2f4de6c54
Ørjan Bostad Vesterlid <orjanbostad.vesterlid@gmail.com>
2019-07-03 15:45:22 +0200

    Cleanup and refactor Balances.tsx

--- src/components/organism/Balances.tsx
+++ src/components/organism/Balances.tsx
@@ -19,71 +19,70 @@ interface IError {
 
 const companyId = 1; // Hardcoded until we get a global company context.
 
+const createBalanceEntriesFromMonth = (month: IMonth) => {
+
+  const monthBalances: { [s: string]: {income: number, expense: number, liquidity: number}; } = {};
+  const sortedBalances = month.balance.sort((a, b) => {
+    if (a.date <= b.date ) {
+      return -1;
+    }
+    return 1;
+  });
+
+  const firstOfMonth = moment({year: month.year, month: month.month, day: 1});
+  monthBalances[firstOfMonth.format('YYYY-MM-DD')] = {
+    expense: 0,
+    income: 0,
+    liquidity: month.start_balance,
+  };
+
+  sortedBalances.forEach(b => {
+    monthBalances[b.date] = {
+      expense: 0,
+      income: 0,
+      liquidity: b.money,
+    };
+  });
+
+  month.transactions.forEach(t => {
+    // TransactionType with capital first letter does not reflect enums from API with lowercase..
+    if (t.type.charAt(0).toUpperCase() + t.type.slice(1) === TransactionType.income) {
+      monthBalances[t.date].income += t.money;
+    } else {
+      monthBalances[t.date].expense += t.money;
+    }
+  });
+
+  const balanceEntries: Array<IBalanceEntry> = [];
+  Object.keys(monthBalances).forEach(be => {
+    balanceEntries.push({
+      date: be,
+      expense: monthBalances[be].expense,
+      income: monthBalances[be].income,
+      liquidity: monthBalances[be].liquidity});
+  });
+  return balanceEntries;
+};
+
 const Balances: React.FC<IProps> = props => {
 
   const [monthChosen, setMonthChosen] = React.useState<moment.Moment>(moment());
   const [entries, setEntries] = React.useState<{[s: string]: Array<IBalanceEntry>}>({});
 
-  const createEntries = (month: IMonth) => {
-
-    const monthBalances: { [s: string]: {income: number, expense: number, liquidity: number}; } = {};
-    const sortedBalances = month.balance.sort((a, b) => {
-      if (a.date <= b.date ) {
-        return -1;
-      }
-      return 1;
-    });
-
-    const firstOfMonth = moment({year: month.year, month: month.month, day: 1});
-    monthBalances[firstOfMonth.format('YYYY-MM-DD')] = {
-      expense: 0,
-      income: 0,
-      liquidity: month.start_balance,
-    };
-
-    sortedBalances.forEach(b => {
-      monthBalances[b.date] = {
-        expense: 0,
-        income: 0,
-        liquidity: b.money,
-      };
-    });
-
-    month.transactions.forEach(t => {
-      // TransactionType with capital first letter does not reflect enums from API with lowercase..
-      if (t.type.charAt(0).toUpperCase() + t.type.slice(1) === TransactionType.income) {
-        monthBalances[t.date].income += t.money;
-      } else {
-        monthBalances[t.date].expense += t.money;
-      }
-    });
-
-    const balanceEntries: Array<IBalanceEntry> = [];
-    Object.keys(monthBalances).forEach(be => {
-      balanceEntries.push({
-        date: be,
-        expense: monthBalances[be].expense,
-        income: monthBalances[be].income,
-        liquidity: monthBalances[be].liquidity});
-    });
-    return balanceEntries;
-  };
-
   React.useEffect(() => {
+    const entryKey = monthChosen.format('YYYY-MM');
 
-    // Fetch month and create balance entries for chosen month if not done already.
-    if (!(`${monthChosen.month()}-${monthChosen.year()}` in entries)) {
+    if (!(entryKey in entries)) {
+      // API is indexing months starting from 1, therefore we need to add 1 to get correct result.
       BalancesAPI.getMonth(monthChosen.month() + 1, monthChosen.year(), companyId)
         .then(balanceEntries => {
+          const newEntries = {...entries};
           if (balanceEntries.length !== 1) {
-            const newEntries = {...entries};
-            newEntries[`${monthChosen.month()}-${monthChosen.year()}`] = [];
-            setEntries(newEntries);
+            newEntries[entryKey] = [];
           } else {
-            const newEntries = {...entries};
-            newEntries[`${monthChosen.month()}-${monthChosen.year()}`] = createEntries(balanceEntries[0]);
-            setEntries(newEntries);
+            newEntries[entryKey] = createBalanceEntriesFromMonth(balanceEntries[0]);
           }
+          setEntries(newEntries);
         })
         .catch(error => {
           alert(error);
@@ -91,7 +90,7 @@ const Balances: React.FC<IProps> = props => {
     }
   }, [monthChosen, entries]);
 
-  const entriesIndex = `${monthChosen.month()}-${monthChosen.year()}`;
+  const entriesIndex = monthChosen.format('YYYY-MM');
   const title = 'Balances';
   const description = 'Showing income, expense and liquidity for a month';
 
