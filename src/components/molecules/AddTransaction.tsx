d00619bb5f454c7659ea1f6c9739c19713d1fff4
Nicolaj Nielsen <nicolaj@kodeworks.no>
2019-07-03 16:09:50 +0200

    formated code in AddTransaction

--- src/components/molecules/AddTransaction.tsx
+++ src/components/molecules/AddTransaction.tsx
@@ -17,10 +17,9 @@ const AddTransaction: React.FC<IProps> = props => {
   const [date, setDate] = React.useState('1970-01-01');
   const [amount, setAmount] = React.useState('');
   const [name, setName] = React.useState('');
-  const [customer, setCustomer] = React.useState('');
+  const [counterpart, setCounterpart] = React.useState('');
   const [notes, setNotes] = React.useState('');
   const [recurring, setRecurring] = React.useState(false);
-
   const [interval, setInterval] = React.useState(1);
   const [intervalType, setIntervalType] = React.useState('Month');
   const [dayOfMonth, setDayOfMonth] = React.useState(23);
@@ -30,7 +29,6 @@ const AddTransaction: React.FC<IProps> = props => {
       intervalValue={interval}
       intervalTypeValue={intervalType}
       DoMValue={dayOfMonth}
-
       setInterval={setInterval}
       setTypeInterval={setIntervalType}
       setDoM={setDayOfMonth}
@@ -40,32 +38,54 @@ const AddTransaction: React.FC<IProps> = props => {
   return (
     <Collapsable heading={<h1>Add new transaction</h1>}>
       <div className={props.className}>
-        <Input value={date} id="date" type="date" setState={setDate}>Date</Input>
-        <Input value={amount} id="amount" type="number" setState={setAmount} placeholder="0.00">Amount</Input>
-        <Input value={name} id="name" type="text" setState={setName} placeholder="John Doe">Name</Input>
-        <Input
-          value={customer}
-          id="customer"
-          type="text"
-          setState={setCustomer}
-          placeholder="Otter Accessories Inc."
-        >
-          Customer
-        </Input>
-        <TextArea
-          value={notes}
-          id="notes"
-          setState={setNotes}
-          placeholder="Notes regarding the transaction"
-        >
-          Notes
-        </TextArea>
-        <Checkbox value={recurring} setState={setRecurring} id="recurring">Recurring?</Checkbox>
-        <br />
-        {recurring && recurringTransactionOptions}
+        <form>
+          <Input value={date} id="date" type="date" setState={setDate}>
+            Date
+          </Input>
+          <Input
+            value={amount}
+            id="amount"
+            type="number"
+            setState={setAmount}
+            placeholder="0.00"
+          >
+            Amount
+          </Input>
+          <Input
+            value={name}
+            id="name"
+            type="text"
+            setState={setName}
+            placeholder="John Doe"
+          >
+            Name
+          </Input>
+          <Input
+            value={counterpart}
+            id="counterpart"
+            type="text"
+            setState={setCounterpart}
+            placeholder="Otter Accessories Inc."
+          >
+            Counterpart
+          </Input>
+          <TextArea
+            value={notes}
+            id="notes"
+            setState={setNotes}
+            placeholder="Notes regarding the transaction"
+          >
+            Notes
+          </TextArea>
+          <Checkbox value={recurring} setState={setRecurring} id="recurring">
+            Recurring?
+          </Checkbox>
+          <br />
+          {recurring && recurringTransactionOptions}
 
-        <OutlinedButton variant="success">Add income</OutlinedButton>
-        <OutlinedButton variant="error">Add expense</OutlinedButton>
+          <OutlinedButton variant="success">Add income</OutlinedButton>
+          <OutlinedButton variant="error">Add expense</OutlinedButton>
+        </form>
       </div>
     </Collapsable>
   );
