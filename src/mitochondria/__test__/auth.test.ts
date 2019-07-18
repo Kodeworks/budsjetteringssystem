859c5e1c6f8d4e09f07ead09ba77fd5dc8334561
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2019-07-18 15:42:38 +0200

    Update auth.test.ts to reflect changes to auth store

--- src/mitochondria/__test__/auth.test.ts
+++ src/mitochondria/__test__/auth.test.ts
@@ -40,15 +40,11 @@ describe('Authentication/Registration', () => {
       loginPassword
     );
     expect(registerResp).toEqual({
-      access,
-      refresh,
-      user: {
-        companies: [],
-        email: loginEmail,
-        first_name: loginFirstName,
-        id: 0,
-        last_name: loginLastName,
-      },
+      companies: [],
+      email: loginEmail,
+      first_name: loginFirstName,
+      id: 0,
+      last_name: loginLastName,
     });
   });
 
