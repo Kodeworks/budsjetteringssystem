d6b22bc170e62eedf9916de1ab84c489b5f934e7
Fredrik August Madsen-Malmo <fredrik@kodeworks.no>
2019-09-05 16:34:17 +0200

    Fetch new user when creating company so it picks up the new company

--- frontend/src/components/pages/Companies.tsx
+++ frontend/src/components/pages/Companies.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import { useCompanyState } from '../../store/contexts/company';
+import { useAuth } from '../../store/contexts/auth';
+import { useCompany } from '../../store/contexts/company';
+import { AuthActions } from '../../store/reducers/auth';
+import { CompanyActions } from '../../store/reducers/company';
+import Input from '../atoms/Input';
 
 const Companies: React.FC = props => {
-  const companies = useCompanyState();
+  const [companies, dispatch] = useCompany();
+  const [auth, authDispatch] = useAuth();
+
+  const [name, setName] = React.useState('');
+  const [orgNr, setOrgNr] = React.useState('');
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await CompanyActions.doCreateCompany({ name, org_nr: orgNr }, dispatch);
+    await AuthActions.doSetUser(auth!.id, authDispatch);
+  };
 
   return (
     <>
@@ -20,10 +35,27 @@ const Companies: React.FC = props => {
         </div>
       ))}
 
-      <form>
-        <input type="text" />
-        <input type="number" />
-        <input type="submit" value="Create new company" />
+      <form onSubmit={onSubmit}>
+        <Input
+          placeholder="Goggle Inc."
+          value={name}
+          setState={setName}
+          id="name"
+          type="text"
+        >
+          Name
+        </Input>
+        <Input
+          placeholder="01189998819991197253"
+          value={orgNr}
+          setState={setOrgNr}
+          id="orgnr"
+          type="text"
+        >
+          Organization number
+        </Input>
+
+        <input type="submit" value="Create" />
       </form>
     </>
   );
