2555608f7221825756b183ebb90e180bbe2077d6
Fredrik August Madsen-Malmo <fredrik@kodeworks.no>
2019-09-20 16:14:29 +0200

    Merge with origin master

--- frontend/src/components/organism/Projections.tsx
+++ frontend/src/components/organism/Projections.tsx
@@ -7,7 +7,6 @@ import { useTransactionState } from '../../store/contexts/transactions';
 import PageTitle from '../atoms/PageTitle';
 
 const Projections: React.FC<{ className?: string }> = ({ className }) => {
-<<<<<<< HEAD
   const store = useTransactionState();
   const auth = useAuthState();
 
@@ -35,9 +34,7 @@ const Projections: React.FC<{ className?: string }> = ({ className }) => {
       )
       .sort((t1, t2) => (t2.date > t1.date ? -1 : 1))
       .map(t => {
-        t.type === 'IN'
-          ? (accumulatedBalance += t.money / 100)
-          : (accumulatedBalance -= t.money / 100);
+        accumulatedBalance += (t.type === 'IN' ? t.money : -t.money) / 100;
         return (
           <tr key={`id${t.id}`}>
             <td>{moment(t.date, moment.ISO_8601).format('DD/MM/YYYY')}</td>
@@ -69,52 +66,6 @@ const Projections: React.FC<{ className?: string }> = ({ className }) => {
         <tbody>{renderTransactions()}</tbody>
       </table>
     </>
-=======
-  const { transactions } = useTransactionState();
-  // Note: currentBalance should be changed to be the incoming balance to the current month (first day of the month)
-  const currentBalance = 0;
-  let accumulatedBalance = currentBalance;
-  // render transactions from first day of current month until five years in the future
-  const renderTransactions = () => {
-    return (
-      <tbody>
-        {transactions
-          .filter(t =>
-            moment(t.date).isBetween(
-              moment().startOf('month'),
-              moment().add(5, 'years')
-            )
-          )
-          .sort((t1, t2) => (t2.date > t1.date ? -1 : 1))
-          .map(t => {
-            accumulatedBalance += (t.type === 'IN' ? t.money : -t.money) / 100;
-            return (
-              <tr key={t.id}>
-                <td>{t.date}</td>
-                <td>{t.description}</td>
-                <td>{t.type === 'IN' ? (t.money / 100).toFixed(2) : ''}</td>
-                <td>{t.type === 'EX' ? (t.money / 100).toFixed(2) : ''}</td>
-                <td>{accumulatedBalance.toFixed(2)}</td>
-              </tr>
-            );
-          })}
-      </tbody>
-    );
-  };
-  return (
-    <table className={className}>
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Description</th>
-          <th>In</th>
-          <th>Out</th>
-          <th>Available</th>
-        </tr>
-      </thead>
-      {renderTransactions()}
-    </table>
->>>>>>> origin
   );
 };
 
