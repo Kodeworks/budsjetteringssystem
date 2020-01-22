2612d3b90eb99cd38b210286518d1922ad0142d0
erikskaar <erik.skar@online.ntnu.no>
2020-01-22 12:56:45 +0100

    Remove SnackBarSymbol and uncomment authentication tests

--- frontend/src/components/atoms/SnackBarSymbol.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-
-export default styled.span`
-  margin-right: 0.9em;
-`;
--- frontend/src/components/organism/__test__/Authentication.test.tsx
+++ frontend/src/components/organism/__test__/Authentication.test.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { cleanup, render } from '../../../helpers/test-utils';
 import Login from '../../organism/authentication/Login';
 import Register from '../../organism/authentication/Register';
-/*
+
 afterEach(cleanup);
 
 test('Login form renders with correct headertext, aria-labels and placeholders', () => {
@@ -15,10 +15,9 @@ test('Login form renders with correct headertext, aria-labels and placeholders',
 });
 
 test('Register form renders with correct headertext, aria-labels and placeholders', () => {
-  const { getByTestId, getByLabelText } = render( <Register />);
+  const { getByTestId, getByLabelText } = render(<Register />);
 
   expect(getByTestId('authform-header')).toHaveTextContent(/^Sign up$/);
   expect(getByLabelText('email')).toHaveAttribute('aria-label', 'email');
   expect(getByLabelText('password')).toHaveAttribute('aria-label', 'password');
 });
-*/
